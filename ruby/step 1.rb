b85d22b9a50b93b27fc39e7903436da1a985e269
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-10 22:30:02 +0700

    промежуточная фиксация

--- ruby/step 1.rb
+++ ruby/step 1.rb
@@ -1,2 +1,48 @@
 # Шпаргалка - Основны языка Ruby с примерами
 # В файлах *.rb будет код, который по тем или иным причинам, неудобно или невозможно выполнить в Jupyter Notebook
+
+
+
+# Пример создания псевдонима
+
+class String
+  alias / :split
+end
+
+
+strings = "строка строка строка строка "
+
+p (strings / " ").size
+
+
+# Перенос по словам
+
+class String
+  def wrap(col = 80)
+      gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/, "\\1\\3\n")
+  end
+end
+
+p "wrapping text with regular expressions".wrap(10)
+
+# Методы преобразования к строке
+# Если нужно, чтобы ваши объекты упорядочивались и сравнивались с обычными строками, следует применять примесь Comparable и единственный специальный метод to_str. Наличие этого метода у вашего объекта — знак для Ruby, что для сравнения следует применять не встроенный в String метод, а ваш.
+
+class Container
+  include Comparable
+  def to_str
+      "контейнер"
+  end
+
+  def to_s
+      "контейнер"
+  end
+
+  def <=>(other)
+      to_s <=> other.to_s
+  end
+end
+
+cont = Container.new
+
+p "контейнер" == cont    #=> true
